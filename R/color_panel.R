1722dd1d42a34b810db513062b18919548d66af1
Peng Xia <xp294053@163.com>
2024-08-15 00:43:57 +0800

    update_240815

---
+++ R/color_panel.R
@@ -0,0 +1,316 @@
+#' @title Default discrete color presets by 'I want hue'
+#' @description Generate color presets from 'I want hue' online tool
+#' @param n How many colors to generate
+#' @param col.space Color space, Options: "default", "intense", "pastel",
+#' "all" (k-Means) or "all_hard" (force vector)
+#' @param set Several random presets, Default: 1
+#' @return Vector of colors
+#' @details
+#' Random color presets generated from: https://medialab.github.io/iwanthue/
+#' @examples
+#' color_iwh(10)
+#' @rdname color_iwh
+#' @export
+
+color_iwh <- function(
+    n,
+    col.space = c("default","intense","pastel","all","all_hard"),
+    set = 1
+) {
+  message_only_once("color_iwh","The 'I want hue' color presets were generated from: https://medialab.github.io/iwanthue/")
+
+  if(!col.space[1] %in% c("default","intense","pastel","all","all_hard", 1:5)) {
+    message('"col.space" should be "default", "intense", "pastel", "all" or "all_hard"\n',
+            'Using "default" preset.')
+    col.space <- "default"
+  }
+  col.space <- col.space[1]
+  if(!is.numeric(n)) stop("'n' should be integer (2-50)")
+  if(n > 20 & n < 31 & col.space %in% c("default",1)) {
+    message('Input color number is ', n, '. Using "intense" preset instead.')
+    col.space <- "intense"
+  }else if(n > 30 & n < 51 & col.space %in% c("default",1)) {
+    message('Input color number is ', n, '. Using "all" preset instead.')
+    col.space <- "all"
+  }else if(n > 50 & col.space %in% c("default",1)) {
+    stop("Input color number is ", n, ". Too many colors (n > 50).")
+  }
+  n.range <- names(presets_color_iwh[[col.space]])
+  if(!n %in% n.range){
+    stop('"n" should be integer and in range (',
+         n.range[1],'~',tail(n.range, 1),') for preset "',col.space,'".')
+  }
+  set.range <- names(presets_color_iwh[[col.space]][[as.character(n)]])
+  if(!set %in% set.range){
+    stop('"set" should be in range (',set.range[1],'~',tail(set.range, 1),') for ',
+         n,' colors in preset "', col.space,'".')
+  }
+  cols <- presets_color_iwh[[col.space]][[as.character(n)]][[set]]
+  return(cols)
+}
+
+#' @title Professional discrete color presets by 'I want hue'
+#' @description Generate professional color presets from 'I want hue' color tool
+#' @param n How many colors to generate
+#' @param col.space Color space, Options: "default", "light", "red", "yellow", "green", "blue", or "purple"
+#' @param sort Sort colors by hue (default) or differentiation, Options: "hue", "diff"
+#' @param set Several random presets, Default: 1
+#' @return Vector of colors
+#' @details
+#' Random color presets generated from: https://medialab.github.io/iwanthue/
+#' @examples
+#' color_pro(10)
+#' @rdname color_pro
+#' @export
+
+color_pro <- function(
+    n,
+    col.space = c("default","light","red","yellow","green","blue","purple"),
+    sort = c("hue","diff"),
+    set = 1
+) {
+  message_only_once("color_iwh","The 'I want hue' color presets were generated from: https://medialab.github.io/iwanthue/")
+
+  if(!col.space[1] %in% c("default","light","red","yellow","green","blue","purple", 1:7)) {
+    message('"col.space" should be "default", "light", "red", "yellow", "green", "blue", or "purple"\nUsing "default" preset.')
+    col.space <- "default"
+  }
+  col.space <- col.space[1]
+
+  if(!sort[1] %in% c("hue","diff", 1:2)) {
+    message('"sort" should be "hue" or "diff". Sorting by "hue"')
+    sort <- "hue"
+  }
+  sort <- sort[1]
+
+  cols_set <- presets_color_pro[[col.space]][[sort]]
+
+  n.range <- names(cols_set)
+  if(!n %in% n.range | !is.numeric(n)){
+    stop('"n" should be integer and in range (',
+         n.range[1],'~',tail(n.range, 1),') for preset "',col.space,'".')
+  }
+  cols_set <- cols_set[[as.character(n)]]
+
+  set.range <- length(cols_set)
+  if(!set %in% 1:set.range){
+    stop('"set" should be in range (1~',set.range,') for ',
+         n,' colors in preset "', col.space,'".')
+  }
+  cols <- cols_set[[set]]
+  return(cols)
+}
+
+#' @title Mix RYB color
+#' @description Mix 3 primary colors (red, yellow and blue) and
+#' return RGB code
+#' @param ryb numeric vector range in [0,1],
+#' e.g. c(r = 0.3, y = 0.5, b = 0.2)
+#' @return RGB code
+#' @details see above
+#' @examples
+#' library(scales)
+#' library(dplyr)
+#' data.frame(
+#'   red = c(1,0,0),
+#'   yellow = c(0,1,0),
+#'   blue = c(0,0,1),
+#'   orange = c(1,1,0),
+#'   purple = c(1,0,1),
+#'   green = c(0,1,1),
+#'   black = c(1,1,1),
+#'   grey = c(0.5,0.5,0.5),
+#'   white = c(0,0,0)
+#' ) %>%
+#'   apply(2, ryb2rgb) %>%
+#'   show_col()
+#' @rdname ryb2rgb
+#' @export
+
+ryb2rgb <- function(ryb = c(0,0,0)){
+  ryb2 <- sca(ryb)
+  x <- ang(ryb2)
+  tar <- seg.x(x)
+  tar <- sca(c(tar,0,1), range = c(1-max(ryb), 1-min(ryb)))[1:3]
+  col <- rgb(tar[1], tar[2], tar[3])
+  return(col)
+}
+
+ryb_ref <- data.frame(
+  # 0 = red, 2 = yellow, 4 = blue, 6 = red
+  x = c(0,1,2,3,4,5,6),
+  r = c(    1,  1,   1,   0,  0, 0.625,    1),
+  y = c(0.125,0.5,0.75,0.75,0.5, 0.125,0.125),
+  b = c(0.125,  0,   0,   0,  1,0.9375,0.125)
+)
+seg <- function(range, weight = c(1,1)){
+  len <- range[,2] - range[,1]
+  rat <- weight[2] / (weight[1] + weight[2])
+  tar <- range[,1] + len * rat
+  return(tar)
+}
+seg.x <- function(x){
+  # x should be 0 to 6
+  x2 <- c(floor(x),ceiling(x))
+  if(x2[1] == x2[2]){
+    res <- as.numeric(ryb_ref[ryb_ref$x == x, 2:4])
+  }else{
+    range <- t(ryb_ref[ryb_ref$x %in% x2, 2:4])
+    res <- seg(range, weight = c(x2[2] - x, x - x2[1]))
+  }
+  return(res)
+}
+sca <- function(vec, range = c(0,1)){
+  len.r <- range[2] - range[1]
+  len.v <- max(vec) - min(vec)
+  if(len.v == 0){
+    res <- rep(mean(range), times = length(vec))
+  }else{
+    rat <- (vec - min(vec)) / len.v
+    res <- range[1] + len.r * rat
+  }
+  return(res)
+}
+ang <- function(ryb = c(0,0.5,1)){
+  # min should be 0 and max should be 1
+  min.ryb <- which.min(ryb)
+  range <- switch(
+    min.ryb,
+    "1" = c(2,4),
+    "2" = c(6,4),
+    "3" = c(0,2)
+  )
+  x <- seg(t(range), ryb[-min.ryb])
+  return(x)
+}
+
+# internal functions for fill/color themes
+
+scale_fill_cont_auto <- function(color_scheme) {
+  if(is.null(color_scheme)) return(NULL)
+  library(ggplot2)
+  if(any(color_scheme %in% c("A","B","C","D","E"))) {
+    import("viridis")
+    cols <- scale_fill_viridis(option = color_scheme)
+  }else if(!is.na(color_scheme["mid"])) {
+    cols <- scale_fill_gradient2(low = color_scheme["low"],
+                                 mid = color_scheme["mid"],
+                                 high = color_scheme["high"])
+  }else if(all(!is.na(color_scheme[c("low","high")]))) {
+    cols <- scale_fill_gradient(low = color_scheme["low"],
+                                high = color_scheme["high"])
+  }else{
+    cols <- scale_fill_gradientn(colors = color_scheme)
+  }
+  return(cols)
+}
+
+scale_color_cont_auto <- function(color_scheme) {
+  if(is.null(color_scheme)) return(NULL)
+  library(ggplot2)
+  if(any(color_scheme %in% c("A","B","C","D","E"))) {
+    import("viridis")
+    cols <- scale_color_viridis(option = color_scheme)
+  }else if(!is.na(color_scheme["mid"])) {
+    cols <- scale_color_gradient2(low = color_scheme["low"],
+                                  mid = color_scheme["mid"],
+                                  high = color_scheme["high"])
+  }else if(all(!is.na(color_scheme[c("low","high")]))) {
+    cols <- scale_color_gradient(low = color_scheme["low"],
+                                 high = color_scheme["high"])
+  }else{
+    cols <- scale_color_gradientn(colors = color_scheme)
+  }
+  return(cols)
+}
+
+scale_color_disc_auto <- function(color_scheme, n, labels = waiver()) {
+  if(is.null(color_scheme)) return(NULL)
+  library(ggplot2)
+  pro_theme <- c("default","light","red","yellow","green","blue","purple")
+  iwh_theme <- c("default","intense","pastel","all","all_hard")
+  if(length(color_scheme) == 1 & n <= 50) {
+    if(color_scheme %in% c("default","light",paste0("pro_",pro_theme))) {
+      color_scheme <- sub("pro_","",color_scheme)
+      if(n == 1 && color_scheme == "default") return(NULL)
+      cols <- color_pro(n, col.space = color_scheme)
+      cols <- scale_color_manual(values = cols, labels = labels)
+    } else if(color_scheme %in% paste0("iwh_",iwh_theme)) {
+      color_scheme <- sub("iwh_","",color_scheme)
+      cols <- color_iwh(n, col.space = color_scheme)
+      cols <- scale_color_manual(values = cols, labels = labels)
+    } else {
+      cols <- scale_color_brewer(palette = color_scheme, labels = labels)
+    }
+  } else {
+    cols <- scale_color_manual(values = color_scheme, labels = labels)
+  }
+  return(cols)
+}
+
+scale_fill_disc_auto <- function(color_scheme, n) {
+  if(is.null(color_scheme)) return(NULL)
+  library(ggplot2)
+  pro_theme <- c("default","light","red","yellow","green","blue","purple")
+  iwh_theme <- c("default","intense","pastel","all","all_hard")
+  if(length(color_scheme) == 1 & n <= 50) {
+    if(color_scheme %in% c("default","light",paste0("pro_",pro_theme))) {
+      color_scheme <- sub("pro_","",color_scheme)
+      if(n == 1 && color_scheme == "default") return(NULL)
+      cols <- color_pro(n, col.space = color_scheme)
+      cols <- scale_fill_manual(values = cols)
+    } else if(color_scheme %in% paste0("iwh_",iwh_theme)) {
+      color_scheme <- sub("iwh_","",color_scheme)
+      cols <- color_iwh(n, col.space = color_scheme)
+      cols <- scale_fill_manual(values = cols)
+    } else {
+      cols <- scale_fill_brewer(palette = color_scheme)
+    }
+  } else {
+    cols <- scale_fill_manual(values = color_scheme)
+  }
+  return(cols)
+}
+
+#' @title Save Custom Color Settings to a Seurat Object
+#' @description This function stores custom color settings within a Seurat object, allowing these settings to be reused across various visualization functions within the Seurat environment.
+#' @param seu A Seurat object to which the color settings will be saved.
+#' @param col_list A list of color settings where names correspond to variable names and values are the colors to be associated with each variable.
+#' @return The modified Seurat object with updated color settings in the `misc` slot.
+#' @details `save_colors` enhances workflow efficiency by centralizing color management within the Seurat object. This enables consistent and coherent visualizations across multiple plots, reducing the need for repeated color specifications and ensuring that plots are both aesthetically pleasing and informative.
+#' @examples
+#' library(SeuratExtend)
+#'
+#' # Define custom colors for different clusters and genes
+#' custom_colors <- list(
+#'   "cluster" = "pro_blue",
+#'   "CD14" = "D",
+#'   "CD3D" = c("#EEEEEE", "black")
+#' )
+#' # Save these colors to the Seurat object
+#' pbmc <- save_colors(pbmc, custom_colors)
+#' # Use the colors in DimPlot2
+#' DimPlot2(pbmc, features = c("cluster", "CD14", "CD3D"))
+#'
+#' @rdname save_colors
+#' @export
+
+save_colors <- function(seu, col_list) {
+  # Ensure that col_list is a list before proceeding
+  if (!is.list(col_list)) {
+    stop("The 'col_list' parameter must be a list. Example format:\n",
+         "list(cluster = 'pro_blue', CD14 = 'D', CD3D = c('#EEEEEE', 'black'))")
+  }
+
+  # Check if 'var_colors' slot exists and is not NULL; if not, initialize as an empty list
+  if (is.null(seu@misc[["var_colors"]])) {
+    seu@misc[["var_colors"]] <- list()
+  }
+
+  # Update 'var_colors' with new colors without overwriting existing entries
+  # Only update the entries provided in col_list
+  seu@misc[["var_colors"]][names(col_list)] <- col_list
+
+  # Optionally return the Seurat object if you want to check the updated list
+  return(seu)
+}
--- R/scVisual_panel.R
+++ R/scVisual_panel.R
@@ -13,6 +13,7 @@
 #' @export
 #' @import Seurat
 #' @import ggplot2
+#' @import patchwork
 #' @examples
 #' \dontrun{
 #' library(Seurat)
@@ -73,7 +74,7 @@ scVisFeaturePlot <- function(scRNA, feature, reduction = "umap", pt.size = 0.000
         ggtitle(x)
     })
 
-    return(aplot::plot_list(plot))
+    return(patchwork::wrap_plots(plot))
   } else {
     # Create the feature plot
     plot <- FeaturePlot(
@@ -1165,11 +1166,441 @@ CenterTitle <- function () {
   return(theme(plot.title = element_text(hjust = 0.5), validate = TRUE))
 }
 
+# =========== 4.scVisVlnPlot =============
+#' @include generics.R
+#'
+NULL
+
+#' @param seu A Seurat object. Only applicable for the Seurat method.
+#' @param group.by A variable from `meta.data` for grouping or a character vector of equal length as the number of cells. Only applicable for the Seurat method.
+#' @param split.by A variable from `meta.data` to bifurcate the violin plots. Only applicable for the Seurat method.
+#' @param cells Cell identifiers for use. Defaults to all cells. Only applicable for the Seurat method.
+#' @param slot Slot to retrieve feature data from. Only applicable for the Seurat method.
+#' @param assay Name of the assay to employ. Defaults to the active assay. Only applicable for the Seurat method.
+#' @param priority If set to "expr", extracts data from the expression matrix over `meta.data`. Only applicable for the Seurat method.
+#' @param load.cols When TRUE, automatically loads pre-stored color information for variables from `seu@misc[["var_colors"]]`.
+#' @rdname scVisVlnPlot
+#' @export
+
+scVisVlnPlot.Seurat <- function(
+    seu,
+    features,
+    group.by = NULL,
+    split.by = NULL,
+    cells = NULL,
+    slot = "data",
+    assay = NULL,
+    priority = c("expr","none"),
+    cols = "auto",
+    load.cols = TRUE,
+    ncol = NULL,
+    lab_fill = "group",
+    scales = "free_y",
+    violin = T,
+    box = T,
+    width = 0.9,
+    pt = T,
+    hide.outlier = F,
+    pt.style = c("jitter","quasirandom"),
+    pt.size = 0.2,
+    pt.alpha = 1,
+    strip.position = "top",
+    stat.method = c("none", "wilcox.test", "t.test"),
+    p.adjust.method = "holm",
+    label = c("p.signif","p","p.adj","p.format"),
+    comparisons = NULL,
+    hide.ns = TRUE,
+    step.increase = 0.12,
+    tip.length = 0.03,
+    ...
+) {
+  Std.matr <- Seu2Matr(
+    seu = seu,
+    features = features,
+    group.by = group.by,
+    split.by = split.by,
+    cells = cells,
+    slot = slot,
+    assay = assay,
+    priority = priority
+  )
+
+  cols <- scVisVlnPlot_SelColDisc(
+    seu = seu,
+    group.by = group.by,
+    split.by = split.by,
+    cols = cols,
+    load.cols = load.cols
+  )
+
+  p <- scVisVlnPlot.default(
+    matr = Std.matr$matr,
+    f = Std.matr$f,
+    f2 = Std.matr$f2,
+    t = T,
+    cols = cols,
+    ncol = ncol,
+    lab_fill = lab_fill,
+    scales = scales,
+    violin = violin,
+    box = box,
+    width = width,
+    pt = pt,
+    hide.outlier = hide.outlier,
+    pt.style = pt.style,
+    pt.size = pt.size,
+    pt.alpha = pt.alpha,
+    strip.position = strip.position,
+    stat.method = stat.method,
+    p.adjust.method = p.adjust.method,
+    label = label,
+    comparisons = comparisons,
+    hide.ns = hide.ns,
+    step.increase = step.increase,
+    tip.length = tip.length,
+    ...)
+  return(p)
+}
+
+#' @param matr A matrix or data frame with rows as features and columns as cells.
+#' @param f A factor or vector indicating the identity of each cell. Should match the column length of `matr`.
+#' @param f2 A factor or vector akin to `f` for splitting the violin plots. Default: NULL.
+#' @param features Features to depict, such as gene expression, metrics, PC scores, or any data obtainable via `FetchData()`. Default: NULL (all features in matrix).
+#' @param t If the matrix has features in columns and cells in rows, transpose the matrix first. Default: FALSE.
+#' @param cols Flexible color settings for the plot, accepting a variety of inputs:
+#'
+#'     - Seven color_pro styles: "default", "light", "pro_red", "pro_yellow", "pro_green", "pro_blue", "pro_purple".
+#'
+#'     - Five color_iwh styles: "iwh_default", "iwh_intense", "iwh_pastel", "iwh_all", "iwh_all_hard".
+#'
+#'     - Brewer color scales as specified by `brewer.pal.info`.
+#'
+#'     - Any manually specified colors.
+#' @param ncol Specifies the number of columns for display if multiple plots are shown. Default: NULL.
+#' @param lab_fill Label for the figure legend. Default: 'group'.
+#' @param scales Scales parameter passed to \code{\link[ggplot2:facet_wrap]{ggplot2::facet_wrap()}}. Default: 'free_y'.
+#' @param violin Indicates whether to generate a violin plot. Default: TRUE.
+#' @param box Indicates whether to depict a box plot. Default: TRUE.
+#' @param width Width of the box plot. Default: 0.9.
+#' @param pt Indicates if points should be plotted. Default: TRUE.
+#' @param hide.outlier Conceals outlier points from the box plot. Default: FALSE.
+#' @param pt.style Position adjustment. Default choices: "jitter", "quasirandom".
+#' @param pt.size Point size setting. Default: 0.2.
+#' @param pt.alpha Adjusts the transparency of points. Default: 1.
+#' @param strip.position Positions the strip ("top" (default), "bottom", "left", or "right"). Only used when `f2 = NULL`.
+#' @param stat.method Determines if pairwise statistics are added to the plot. Either "wilcox.test" or "t.test". Default: "none".
+#' @param p.adjust.method Method for adjusting p-values, especially when conducting multiple pairwise tests or dealing with multiple grouping variables. Options include "holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr", and "none". Note: Adjustments are independently conducted for each variable in formulas containing multiple variables. Default: 'holm'.
+#' @param label Specifies label type. Options include "p.signif" (showing significance levels), "p.format" (formatted p value), or "p", "p.adj". Default: "p.signif".
+#' @param comparisons List of length-2 vectors, each containing either names of two x-axis values or two integers pointing to groups of interest for comparison. Default: all groups.
+#' @param hide.ns If TRUE, the 'ns' symbol is concealed when displaying significance levels. Default: TRUE.
+#' @param step.increase Numeric vector denoting the increase in fraction of total height for each additional comparison, minimizing overlap. Default: 0.12.
+#' @param tip.length Numeric vector indicating the fraction of total height the bar descends to specify the exact column. For a line display instead of a bracket, set to 0. Default: 0.03.
+#' @param ... Further arguments passed to the \code{\link[ggpubr:stat_pvalue_manual]{ggpubr::stat_pvalue_manual()}}.
+#' @rdname scVisVlnPlot
+#' @export
+
+scVisVlnPlot.default <- function(
+    matr, f, f2 = NULL,
+    features = NULL,
+    t = F,
+    cols = "pro_default",
+    ncol = NULL,
+    lab_fill = "group",
+    scales = "free_y",
+    violin = T,
+    box = T,
+    width = 0.9,
+    pt = T,
+    hide.outlier = F,
+    pt.style = c("jitter","quasirandom"),
+    pt.size = 0.2,
+    pt.alpha = 1,
+    strip.position = "top",
+    stat.method = c("none", "wilcox.test", "t.test"),
+    p.adjust.method = "holm",
+    label = c("p.signif","p","p.adj","p.format"),
+    comparisons = NULL,
+    hide.ns = TRUE,
+    step.increase = 0.12,
+    tip.length = 0.03,
+    ...
+) {
+
+  scores <- scVisVlnPlot_Calc(
+    matr = matr,
+    f = f,
+    f2 = f2,
+    features = features,
+    t = t
+  )
+
+  p <- scVisVlnPlot_Plot(
+    scores = scores,
+    cols = cols,
+    ncol = ncol,
+    lab_fill = lab_fill,
+    scales = scales,
+    violin = violin,
+    box = box,
+    width = width,
+    pt = pt,
+    hide.outlier = hide.outlier,
+    pt.style = pt.style,
+    pt.size = pt.size,
+    pt.alpha = pt.alpha,
+    strip.position = strip.position
+  )
+
+  p <- scVisVlnPlot_Stat(
+    p = p,
+    stat.method = stat.method,
+    p.adjust.method = p.adjust.method,
+    label = label,
+    comparisons = comparisons,
+    hide.ns = hide.ns,
+    step.increase = step.increase,
+    tip.length = tip.length,
+    ...
+  )
+
+  return(p)
+}
+
+#' @title StackedViolin
+#' @description Alias of \code{\link[SeuratExtend:scVisVlnPlot]{scVisVlnPlot()}}
+#' @seealso \code{\link[SeuratExtend:scVisVlnPlot]{scVisVlnPlot()}}
+#' @rdname StackedViolin
+#' @export
+
+StackedViolin <- scVisVlnPlot.default
+
+
+#' @rdname StackedViolin
+#' @export
+
+StackedViolin_v3 <- scVisVlnPlot.Seurat
+
+# Internal ----------------------------------------------------------------
+
+scVisVlnPlot_Calc <- function(
+    matr,
+    f,
+    f2,
+    features,
+    t
+) {
+  library(reshape2)
+  if(!t) matr <- t(matr)
+  features <- features %||% colnames(matr)
+  if(length(setdiff(features, colnames(matr))) > 0){
+    message(paste0(setdiff(features, colnames(matr)), collapse = ", "), " not found")
+    features <- intersect(features, colnames(matr))
+  }
+  f <- factor(f)
+  f2 <- f2 %||% data.frame(row.names = rownames(matr))
+  scores <- cbind(f, f2, as.data.frame(matr[,features,drop = F]))
+  scores <- melt(scores, measure.vars = features, variable.name = "feature")
+  return(scores)
+}
+
+scVisVlnPlot_Plot <- function(
+    scores,
+    cols,
+    ncol,
+    lab_fill,
+    scales,
+    violin,
+    box,
+    width,
+    pt,
+    hide.outlier,
+    pt.style,
+    pt.size,
+    pt.alpha,
+    strip.position
+) {
+  library(ggplot2)
+  x <- ifelse(!"f2" %in% colnames(scores), "f", "f2")
+  p <- ggplot(scores, aes(x = .data[[x]], y = value))
+  n <- nlevels(factor(scores[[x]]))
+
+  if(violin) {
+    p <- p + geom_violin(mapping = aes(fill = .data[[x]]), scale = "width", width = width)
+  }
+  if(box & !violin) {
+    if(pt | hide.outlier) {
+      p <- p + geom_boxplot(mapping = aes(fill = .data[[x]]), outlier.shape = NA, width = width)
+    } else {
+      p <- p + geom_boxplot(mapping = aes(fill = .data[[x]]), outlier.size = pt.size, width = width)
+    }
+  }
+  if(pt) {
+    pt.style <- pt.style[1]
+    if(!pt.style %in% c("quasirandom", "jitter")) stop('"pt.style" shoule be "quasirandom" or "jitter"')
+    if(pt.style == "jitter") p <- p + geom_jitter(width = width/2.2, size = pt.size, alpha= pt.alpha)
+    if(pt.style == "quasirandom") {
+      import("ggbeeswarm")
+      p <- p + geom_quasirandom(size = pt.size, width = width/2, alpha= pt.alpha)
+    }
+  }
+  if(box & violin) {
+    if(pt | hide.outlier) {
+      p <- p + geom_boxplot(outlier.shape = NA, width = 0.12, fill = "white")
+    } else {
+      p <- p + geom_boxplot(fill = "white", outlier.size = pt.size, width = 0.12, outlier.alpha = pt.alpha)
+    }
+  }
+
+  p <- p + scale_fill_disc_auto(color_scheme = cols, n = n)
+
+  if(x == "f"){
+    p <- p +
+      facet_wrap(vars(feature), ncol = ncol, strip.position=strip.position, scales = scales)+
+      ylab(NULL) +
+      xlab(NULL) +
+      theme_classic() +
+      theme(strip.background = element_blank(),
+            strip.placement = "outside",
+            legend.position = "none",
+            axis.text.x = element_text(angle = 45,hjust = 1),
+            strip.text = element_text(face = "bold", size = 10)) +
+      labs(fill = lab_fill) +
+      scale_y_continuous(expand = expansion(mult = c(0,0.08)))
+  }else{
+    p <- p +
+      facet_grid(vars(feature), vars(f), switch = c("both"), scales = scales)+
+      ylab(NULL) +
+      xlab(NULL) +
+      theme_classic() +
+      theme(strip.background = element_blank(),
+            strip.placement = "outside",
+            axis.text.x = element_blank(),
+            strip.text.y = element_text(face = "bold", size = 10)) +
+      labs(fill = lab_fill) +
+      scale_y_continuous(expand = expansion(mult = c(0,0.08)))
+  }
+  return(p)
+}
+
+scVisVlnPlot_Stat <- function(
+    p,
+    stat.method = c("none", "wilcox.test", "t.test"),
+    p.adjust.method = "holm",
+    label = c("p.signif","p","p.adj","p.format"),
+    comparisons = NULL,
+    hide.ns = TRUE,
+    step.increase = 0.12,
+    tip.length = 0.03,
+    ...
+) {
+  stat.method <- stat.method[1]
+  if(stat.method %in% c("wilcox.test", "t.test")) {
+    library(ggpubr)
+    library(dplyr)
+
+    scores <- p$data
+
+    if (!"f2" %in% colnames(scores)) {
+      formula <- value ~ f
+      group_by_arg <- "feature"
+    } else {
+      formula <- value ~ f2
+      group_by_arg <- c("feature", "f")
+    }
+    stat.test <- compare_means(
+      formula,
+      data = scores,
+      method = stat.method,
+      group.by = group_by_arg,
+      p.adjust.method = p.adjust.method
+    )
+
+    if(hide.ns == TRUE) {
+      stat.test <- filter(stat.test, p.signif != "ns")
+      if(nrow(stat.test) == 0) {
+        message("No statistical significance.")
+        return(p)
+      }
+    }
+    stat.test$groups <- apply(stat.test, 1, function(x) c(x[["group1"]], x[["group2"]]), simplify = F)
+    if(!is.null(comparisons)) {
+      level_comparisons <- lapply(comparisons, function(pair) {
+        if(is.numeric(pair)) {
+          if (!"f2" %in% colnames(scores)) {
+            levels(scores$f)[pair]
+          } else {
+            levels(scores$f2)[pair]
+          }
+        } else if(is.character(pair)) {
+          pair
+        } else {
+          stop("Invalid comparison type")
+        }
+      })
+      stat.test <- filter(stat.test, groups %in% level_comparisons)
+    }
+    stat.test <- scVisVlnPlot_Stat_add_y(stat.test, scores = scores, step.increase = step.increase)
+    p <- p +
+      stat_pvalue_manual(
+        stat.test,
+        label = label[1],
+        tip.length = tip.length,
+        ...)
+  }
+  return(p)
+}
+
+scVisVlnPlot_Stat_add_y <- function(stat.test, scores, step.increase) {
+  summary_data <- scores %>%
+    group_by(feature) %>%
+    dplyr::summarize(
+      min_value = min(value, na.rm = TRUE),
+      max_value = max(value, na.rm = TRUE)
+    )
+  summary_data <- summary_data %>%
+    dplyr::mutate(
+      step = (max_value - min_value) * step.increase,
+      start = (max_value - min_value) * 0.1 + max_value
+    )
+  grouping_cols <- if ("f2" %in% colnames(scores)) c("feature", "f") else "feature"
+  stat.test <- stat.test %>%
+    left_join(summary_data, by = "feature") %>%
+    group_by(across(all_of(grouping_cols))) %>%
+    dplyr::mutate(y.position = start + step * (row_number() - 1)) %>%
+    select(-min_value, -max_value, -step, -start) %>%
+    ungroup()
+  return(stat.test)
+}
+
+scVisVlnPlot_SelColDisc <- function(
+    seu,
+    group.by,
+    split.by,
+    cols,
+    load.cols
+) {
+  if(is.null(cols)) return(NULL)
+  if(cols[1] != "auto") return(cols)
+  if(is.null(group.by)) group.by <- "ident"
+  if(!is.null(split.by)) {
+    var <- split.by
+  } else {
+    var <- group.by
+  }
+  if(length(var) == 1) {
+    load_var <- seu@misc[["var_colors"]][[var]]
+    if(!is.null(load_var)) {
+      cols <- load_var
+    } else {
+      cols <- "pro_default"
+    }
+  }
+  return(cols)
+}
 
 
 
-
-# ============ 4. scVisTissueOR ==========
+# ============ 5. scVisTissueOR ==========
 do.tissueDist <- function(cellInfo.tb = cellInfo.tb,
                           meta.cluster = cellInfo.tb$meta.cluster,
                           colname.patient = "patient",
@@ -1377,7 +1808,7 @@ scVisTissueOR <- function(scRNA, group = 'orig.ident',
 # scVisTissueOR(sce, group = "group", celltype = "celltype", output_dir = "./output_figure", output_file = "tissue_OR.pdf", width = 5, height = 4)
 
 
-# ============ 5. CPDB Visualization =============
+# ============ 6. CPDB Visualization =============
 
 
 #' @title CPDB Visualization
